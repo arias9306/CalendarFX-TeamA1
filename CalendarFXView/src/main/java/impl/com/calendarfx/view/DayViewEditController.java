4272b94b12ff17f66e120cd05fe235ca2139832c
Andrés Arias <arias9306@gmail.com>
2018-09-21 15:08:19 -0500

    - Refactoring code - make possible to overrride the DayViewEditController - Fix an error in the messages_es.properties

--- CalendarFXView/src/main/java/impl/com/calendarfx/view/DayViewEditController.java
+++ CalendarFXView/src/main/java/impl/com/calendarfx/view/DayViewEditController.java
@@ -144,7 +144,7 @@ public class DayViewEditController {
         }
     }
 
-    private void mouseMoved(MouseEvent evt) {
+    protected void mouseMoved(MouseEvent evt) {
         if (!dragging) {
             initDragModeAndHandle(evt);
         }
@@ -172,7 +172,7 @@ public class DayViewEditController {
         }
     }
 
-    private void mousePressed(MouseEvent evt) {
+    protected void mousePressed(MouseEvent evt) {
         dragMode = null;
         handle = null;
 
@@ -277,7 +277,7 @@ public class DayViewEditController {
         }
     }
 
-    private void mouseReleased(MouseEvent evt) {
+    protected void mouseReleased(MouseEvent evt) {
         if (!evt.getButton().equals(MouseButton.PRIMARY) || dayEntryView == null
                 || dragMode == null || !dragging) {
             return;
@@ -377,7 +377,7 @@ public class DayViewEditController {
                         .equals(LocalTime.of(23, 59, 59, 999000000)));
     }
 
-    private void mouseDragged(MouseEvent evt) {
+    protected void mouseDragged(MouseEvent evt) {
         if (!evt.getButton().equals(MouseButton.PRIMARY) || dayEntryView == null
                 || dragMode == null || !dragging) {
             return;
